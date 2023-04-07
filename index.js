0ade3fc034222b06947a194011fbc9158d007546
AJAY PS <psajay37@gmail.com>
2023-04-07 11:59:00 +0530

    added 10 more question to the array.Changed port from 3000 to 5000

--- index.js
+++ index.js
@@ -1,17 +1,125 @@
 const express = require('express')
 const app = express()
-const port = 3001
+const port = 5000;
 
 const USERS = [];
 
 const QUESTIONS = [{
-    title: "Two states",
-    description: "Given an array , return the maximum of the array?",
-    testCases: [{
-        input: "[1,2,3,4,5]",
-        output: "5"
-    }]
-}];
+  title: "Two states",
+  description: "Given an array , return the maximum of the array?",
+  testCases: [{
+  input: "[1,2,3,4,5]",
+  output: "5"
+  }]
+  },
+  {
+  title: "Reverse a string",
+  description: "Given a string, return the string in reverse order.",
+  testCases: [{
+  input: "hello",
+  output: "olleh"
+  },
+  {
+  input: "world",
+  output: "dlrow"
+  }]
+  },
+  {
+  title: "Factorial",
+  description: "Given a number, return the factorial of that number.",
+  testCases: [{
+  input: "5",
+  output: "120"
+  },
+  {
+  input: "3",
+  output: "6"
+  }]
+  },
+  {
+  title: "Fibonacci sequence",
+  description: "Given a number, return the Fibonacci sequence up to that number.",
+  testCases: [{
+  input: "10",
+  output: "[0, 1, 1, 2, 3, 5, 8]"
+  },
+  {
+  input: "5",
+  output: "[0, 1, 1, 2, 3]"
+  }]
+  },
+  {
+  title: "Palindrome",
+  description: "Given a string, return true if it is a palindrome and false if it is not.",
+  testCases: [{
+  input: "racecar",
+  output: "true"
+  },
+  {
+  input: "hello",
+  output: "false"
+  }]
+  },
+  {
+  title: "Sum of multiples",
+  description: "Given two numbers, find the sum of all the multiples of the first number up to the second number.",
+  testCases: [{
+  input: "3, 10",
+  output: "18"
+  },
+  {
+  input: "5, 20",
+  output: "50"
+  }]
+  },
+  {
+  title: "Array reverse",
+  description: "Given an array, return the array in reverse order.",
+  testCases: [{
+  input: "[1, 2, 3, 4, 5]",
+  output: "[5, 4, 3, 2, 1]"
+  },
+  {
+  input: "['a', 'b', 'c']",
+  output: "['c', 'b', 'a']"
+  }]
+  },
+  {
+  title: "Odd or even",
+  description: "Given a number, return whether it is odd or even.",
+  testCases: [{
+  input: "5",
+  output: "odd"
+  },
+  {
+  input: "8",
+  output: "even"
+  }]
+  },
+  {
+  title: "Reverse words",
+  description: "Given a sentence, return the sentence with the words in reverse order.",
+  testCases: [{
+  input: "The quick brown fox",
+  output: "fox brown quick The"
+  },
+  {
+  input: "Hello world",
+  output: "world Hello"
+  }]
+  },
+  {
+  title: "Sum of digits",
+  description: "Given a number, return the sum of its digits.",
+  testCases: [{
+  input: "123",
+  output: "6"
+  },
+  {
+  input: "456",
+  output: "15"
+  }]
+  }];
 
 
 const SUBMISSION = [
